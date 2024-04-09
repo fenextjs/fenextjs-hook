0a32c159920791cec342fe35f0b703cf6bf313eb
Francisco Blanco <blancofrancisco34@gmail.com>
2024-04-09 18:32:12 -0500

    flb

--- cjs/useModal.d.ts
+++ cjs/useModal.d.ts
@@ -1,11 +1,12 @@
 export interface useModalProps {
+    name?: string;
     active?: boolean;
     defaultActive?: boolean;
     onActive?: () => void;
     onClose?: () => void;
     onChange?: (d: boolean) => void;
 }
-export declare const useModal: ({ active: activeProps, defaultActive: defaultActiveProps, onActive: onActiveProps, onChange: onChangeProps, onClose: onCloseProps, }: useModalProps) => {
+export declare const useModal: ({ name, active: activeProps, defaultActive: defaultActiveProps, onActive: onActiveProps, onChange: onChangeProps, onClose: onCloseProps, }: useModalProps) => {
     active: boolean;
     onChange: (d: boolean) => void;
     onActive: () => void;
--- cjs/useModal.js
+++ cjs/useModal.js
@@ -2,11 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.useModal = void 0;
 const react_1 = require("react");
-const useModal = ({ active: activeProps, defaultActive: defaultActiveProps, onActive: onActiveProps, onChange: onChangeProps, onClose: onCloseProps, }) => {
+const useAction_1 = require("./useAction");
+const useModal = ({ name, active: activeProps, defaultActive: defaultActiveProps, onActive: onActiveProps, onChange: onChangeProps, onClose: onCloseProps, }) => {
     const [active, setActive] = (0, react_1.useState)(defaultActiveProps ?? false);
+    const { onAction } = (0, useAction_1.useAction)({
+        name: name ?? "fenext-modal",
+        onActionExecute: name
+            ? (e) => {
+                setActive(e ?? false);
+            }
+            : undefined,
+    });
     const onChange = (d) => {
         onChangeProps?.(d);
         setActive(d);
+        onAction(d);
     };
     const onActive = () => {
         onChange(true);
