c073438412e3b6d26c4371a1afee6eae1eccd3b6
Francisco Blanco <blancofrancisco34@gmail.com>
2024-07-17 14:28:17 -0500

    add validatorData

--- src/useData.tsx
+++ src/useData.tsx
@@ -346,6 +346,9 @@ export const useData = <T, M = any, RT = void, RM = void, ET = any, EM = any>(
             }
         }
     }, [defaultData, options]);
+
+    const validatorData = useMemo(() => options?.validator?.getObjectValidator?.(), [options?.validator])
+
     return {
         data,
         onChangeData,
@@ -363,6 +366,7 @@ export const useData = <T, M = any, RT = void, RM = void, ET = any, EM = any>(
         onReloadKeyData,
 
         validator: options?.validator,
+        validatorData,
         validatorMemo: options?.validatorMemo,
 
         isValidData,
