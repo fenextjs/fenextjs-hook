c6bd32d01d339b5dc1c39f8cf323ea4b2b00fdd5
franciscoblancojn <blancofrancisco34@gmail.com>
2024-10-08 13:23:28 -0500

    fix open modal in beforeunload

--- src/useModal.tsx
+++ src/useModal.tsx
@@ -46,6 +46,7 @@ export const useModal = ({
         }
         window.addEventListener("beforeunload", () => {
             setLocalStorage([]);
+            setActive(false)
         });
     };
     useEffect(onLoadWindows, []);
