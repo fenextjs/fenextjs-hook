c16c083f2d317be70a5a15e77b39a9283b0310a6
franciscoblancojn <blancofrancisco34@gmail.com>
2024-10-28 19:01:10 -0500

    flb

--- cjs/useDocumentEvent.d.ts
+++
@@ -1,8 +0,0 @@
-export type TypeListenerKeyFunctions = keyof DocumentEventMap;
-export type TypeListenerFunctions<K extends TypeListenerKeyFunctions> = (ev: DocumentEventMap[K]) => any;
-export type useDocumentEventProps<K extends TypeListenerKeyFunctions> = {
-    [id in TypeListenerKeyFunctions]?: TypeListenerFunctions<K>;
-};
-export declare const useDocumentEvent: <K extends keyof DocumentEventMap = keyof DocumentEventMap>({ ...props }: useDocumentEventProps<K>) => {
-    onReload: () => void;
-};
