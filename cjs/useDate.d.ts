adea92f254384dd5e4c3ff2b5aa3cb0ac84b3e40
franciscoblancojn <blancofrancisco34@gmail.com>
2024-11-08 15:50:54 -0500

    flb

--- cjs/useDate.d.ts
+++
@@ -1,4 +0,0 @@
-import { FenextjsDate, FenextjsDateProps } from "fenextjs-date";
-export interface useDateProps extends FenextjsDateProps {
-}
-export declare const useDate: ({ ...props }: useDateProps) => FenextjsDate;
