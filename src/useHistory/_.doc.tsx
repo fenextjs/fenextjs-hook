3fde51ffb7b63dc06fc1fb1ffab68da153198481
franciscoblancojn <blancofrancisco34@gmail.com>
2024-11-21 18:10:19 -0500

    add useHistory

---
+++ src/useHistory/_.doc.tsx
@@ -0,0 +1,69 @@
+export default {
+    id: "useHistory",
+    name: "useHistory",
+    description:
+        "El hook `useHistory` gestiona un historial de rutas personalizadas utilizando `sessionStorage`. Proporciona funcionalidades para rastrear las rutas visitadas y retroceder a una ruta específica.",
+    props: [
+        {
+            id: "name",
+            type: "string",
+            require: false,
+            default: `"fenextjs-history"`,
+            description:
+                "Clave utilizada para almacenar el historial de rutas en el almacenamiento de sesión.",
+        },
+    ],
+    returns: [
+        {
+            id: "paths",
+            type: "string[] | undefined",
+            description: "Lista de rutas almacenadas en el historial.",
+        },
+        {
+            id: "currentPath",
+            type: "string | undefined",
+            description:
+                "Ruta actual basada en el historial almacenado. Corresponde a la última ruta registrada.",
+        },
+        {
+            id: "onBack",
+            type: "(props: useHistoryOnBackProps) => void",
+            description:
+                "Función para retroceder a una ruta anterior en el historial.",
+        },
+    ],
+    extrasReturns: [
+        {
+            id: "useHistoryOnBackProps",
+            title: "useHistoryOnBackProps",
+            description:
+                "Propiedades para el uso de onBack:",
+            tableItems: [
+                {
+                    Nombre: "onValidateRuteBack",
+                    Requerido:"no",
+                    Descripcion:
+                        "Función para validar si la ruta de destino es valida para redireccionar.",
+                },
+            ],
+        },
+    ],
+    useExample: [
+        {
+            text: "Inicializar el historial de rutas",
+            content: `const { paths, currentPath } = useHistory();`,
+        },
+        {
+            text: "Retroceder a una ruta previa",
+            content: `const { onBack } = useHistory();
+onBack({});`,
+        },
+        {
+            text: "Retroceder a una ruta previa restringiendo la pagina '/restricted'",
+            content: `const { onBack } = useHistory();
+onBack({
+    onValidateRuteBack: (path) => path !== "/restricted",
+});`,
+        },
+    ],
+};
